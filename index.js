57ca4e3ddb1d6d3c4797cab7d289e2ae9ab9c399
Tobias Schneider <schneider@synyx.de>
2014-07-28 15:54:50 +0200

    add security module with a bounding box

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ exports.EKF = require('./lib/EKF');
 exports.Camera = require('./lib/Camera');
 exports.Controller = require('./lib/Controller');
 exports.Mission = require('./lib/Mission');
+exports.Security = require('./lib/Security');
 
 exports.control = function (client, options) {
     return new autonomy.Controller(client, options);
@@ -12,9 +13,8 @@ exports.control = function (client, options) {
 
 exports.createMission = function (options) {
     var client = ardrone.createClient(options);
-    var control = new autonomy.Controller(client, options);
-    var mission = new autonomy.Mission(client, control, options);
+    var security = new autonomy.Security();
+    var control = new autonomy.Controller(client, security, options);
 
-    return mission;
+    return new autonomy.Mission(client, control, options);
 };
-
